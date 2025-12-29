1f1292078045407005c8f43262a2f53e8af25acd
Kuldeep Solanki <kuldeepsolanki1854@gmail.com>
2025-12-29 22:24:13 +0530

    setup basic controllers and routes

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
-import { connectDB } from "./config/database.js";
+import { connectDB } from "./config/database.config.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 
@@ -10,8 +11,13 @@ const PORT = process.env.PORT || 5000;
 
 connectDB();
 
-app.get("/", (req, res) => {
-  res.json({
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+
+app.use("/api/v1/auth", authRoutes);
+
+app.get("/api/v1/health", (req, res) => {
+  res.status(200).json({
     message: "PurpleMerit API Server is running!",
   });
 });
